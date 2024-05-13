ac68afa23a8003e02574e29c7fc7005cbd346d71
Chris Lennon <lennonc@gmail.com>
2024-05-13 16:24:56 -0500

    include nvidiachat

--- rag.py
+++ rag.py
@@ -2,8 +2,8 @@ from operator import itemgetter
 
 from langchain_community.vectorstores.pgvector import PGVector
 from langchain_community.embeddings import FastEmbedEmbeddings
-# from langchain_community.llms import VLLMOpenAI
 from langchain_openai import ChatOpenAI
+from langchain_nvidia_ai_endpoints import ChatNVIDIA
 from langchain.schema.output_parser import StrOutputParser
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_community.document_loaders import PyPDFLoader
@@ -67,22 +67,16 @@ class ChatCSV:
         - A PromptTemplate for constructing prompts with placeholders for question and context.
         """
 
-        self.model = ChatOpenAI(
-            model=self.llm,
-            openai_api_key="EMPTY",
-            openai_api_base=self.vllmhost,
-            max_tokens=self.token,
-            temperature=self.temp,
-        )
+        # self.model = ChatOpenAI(
+        #     model=self.llm,
+        #     openai_api_key="EMPTY",
+        #     openai_api_base=self.vllmhost,
+        #     max_tokens=self.token,
+        #     temperature=self.temp,
+        # )
+        self.model = ChatNVIDIA(model="mistral_7b")
         # Initialize the RecursiveCharacterTextSplitter with specific chunk settings.
         # Your tone should be professional and informative
-        # self.prompt = ChatPromptTemplate.from_messages(
-        #     messages=[
-        #         SystemMessagePromptTemplate.from_template("You are a knowledgeable chatbot, here to help with questions of the user. Your tone should be professional and informative."),
-        #         MessagesPlaceholder(variable_name="chat_history"),
-        #         HumanMessagePromptTemplate.from_template("{question}"),
-        #     ]
-        # )
         self.prompt = PromptTemplate.from_template(
             """
             <s> [INST] You are an assistant for question-answering tasks. Use the following pieces of retrieved context 
